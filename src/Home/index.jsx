150fd2a11a73afb9fa51cd2ba3c80954d715eef8
son1122 <wongsatorn.n@hotmail.com>
2023-01-11 14:29:54 +0700

    edit init

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,51 +1,117 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
 import React from 'react';
-import DocumentTitle from 'react-document-title';
 import { enquireScreen } from 'enquire-js';
-import Header from './Header';
-import Banner from './Banner';
-import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
-import Page4 from './Page4';
-import Footer from './Footer';
-import './static/style';
 
+import Nav3 from './Nav3';
+import Banner1 from './Banner1';
+import Content0 from './Content0';
+import Content5 from './Content5';
+import Content3 from './Content3';
+import Feature5 from './Feature5';
+import Footer1 from './Footer1';
 
-let isMobile = false;
+import {
+  Nav30DataSource,
+  Banner10DataSource,
+  Content00DataSource,
+  Content50DataSource,
+  Content30DataSource,
+  Feature50DataSource,
+  Footer10DataSource,
+} from './data.source';
+import './less/antMotionStyle.less';
+
+let isMobile;
 enquireScreen((b) => {
   isMobile = b;
 });
 
+const { location } = window;
 
-class Home extends React.PureComponent {
-  state = {
-    isMobile,
-    showShadow: false,
-  };
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+      show: !location.port, // 如果不是 dva 2.0 请删除
+    };
+  }
 
   componentDidMount() {
+    // 适配手机屏幕;
     enquireScreen((b) => {
-      this.setState({
-        isMobile: !!b,
-      });
+      this.setState({ isMobile: !!b });
     });
+    // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
+    /* 如果不是 dva 2.0 请删除 start */
+    if (location.port) {
+      // 样式 build 时间在 200-300ms 之间;
+      setTimeout(() => {
+        this.setState({
+          show: true,
+        });
+      }, 500);
+    }
+    /* 如果不是 dva 2.0 请删除 end */
   }
-  navToShadow = (e) => {
-    this.setState({ showShadow: e.mode === 'leave' });
-  }
+
   render() {
+    const children = [
+      <Nav3
+        id="Nav3_0"
+        key="Nav3_0"
+        dataSource={Nav30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner1
+        id="Banner1_0"
+        key="Banner1_0"
+        dataSource={Banner10DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content0
+        id="Content0_0"
+        key="Content0_0"
+        dataSource={Content00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content5
+        id="Content5_0"
+        key="Content5_0"
+        dataSource={Content50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content3
+        id="Content3_0"
+        key="Content3_0"
+        dataSource={Content30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature5
+        id="Feature5_0"
+        key="Feature5_0"
+        dataSource={Feature50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer1
+        id="Footer1_0"
+        key="Footer1_0"
+        dataSource={Footer10DataSource}
+        isMobile={this.state.isMobile}
+      />,
+    ];
     return (
-      [
-        <Header key="header" className={this.state.showShadow ? 'show-shadow' : ''} />,
-        <Banner key="banner" isMobile={this.state.isMobile} navToShadow={this.navToShadow} />,
-        <Page1 key="page1" isMobile={this.state.isMobile} />,
-        <Page2 key="page2" isMobile={this.state.isMobile} />,
-        <Page3 key="page3" isMobile={this.state.isMobile} />,
-        <Page4 key="page4" />,
-        <Footer key="footer" />,
-        <DocumentTitle title="蚂蚁体验云" key="title" />,
-      ]
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+        {/* 如果不是 dva 2.0 替换成 {children} start */}
+        {this.state.show && children}
+        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      </div>
     );
   }
 }
-export default Home;
