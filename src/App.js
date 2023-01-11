bb71e279829849d518171449176234a7b3a4ae8d
son1122 <wongsatorn.n@hotmail.com>
2023-01-11 14:29:54 +0700

    edit init

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import {enquireScreen} from 'enquire-js';
-import Header from './Home/Nav0';
-import Footer from './Home/Footer0';
-import Home from './Home';
-
-import {
-    Nav00DataSource,
-    Footer00DataSource,
-} from './Home/data.source.js';
-
-let isMobile;
-enquireScreen((b) => {
-    isMobile = b;
-});
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMobile,
-        };
-    }
-
-    componentDidMount() {
-        // Support for mobile resolutions
-        enquireScreen((b) => {
-            this.setState({isMobile: !!b});
-        });
-    }
-
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Header dataSource={Nav00DataSource} isMobile={this.isMobile}/>
-                    <Route exact path="/" component={Home}/>
-                    <Footer dataSource={Footer00DataSource} isMobile={this.isMobile}/>
-                </div>
-            </Router>
-        );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
